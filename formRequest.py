3e6be114bda8c373e0db16d5bcfc2e35f27db5c7
Matvei Malakhov <matveimalakhov@gmail.com>
2022-12-27 12:30:55 +0400

    Added_class_form_Request

---
+++ formRequest.py
@@ -0,0 +1,8 @@
+from person import newUser
+class FormRequest():
+    def __init__(self, numder):
+        #тут должно быть открытие файла из быза с описанием задания
+        self.Worker = newUser.userName
+        self.NameTask
+        self.Discription
+        self.maybe_file
