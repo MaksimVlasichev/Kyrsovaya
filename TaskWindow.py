ebda7e721b71c98a02685c562de98f9aba61ee74
Matvei Malakhov <matveimalakhov@gmail.com>
2022-12-27 12:54:33 +0400

    add_self.status_and_self.remove

--- TaskWindow.py
+++ TaskWindow.py
@@ -1,7 +1,10 @@
 import wx
+import random
 class TaskWindow(wx.Frame):
     def __init__(self, name, discriprion):
         wx.Frame.__init__(self, None, title=name)#окно задания
         self.name = name
         self.discription = discriprion
+        self.Status = random.randint(1,5)
+        self.remove = random.randint(0,1)
         
